06707cbc956add4d05dec68ebac4f4af9da25bf0
barchinsky <barchinsky@gmail.com>
2013-07-18 15:30:35 +0300

    Added makeXml() function.

--- NetworkManager.py
+++ NetworkManager.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 
 from pysnmp.entity.rfc3413.oneliner import cmdgen
+import xml.etree.ElementTree as ET
+#import DeviceActivi
+from lxml import etree
 
 class NetworkManager:
     def __init__(self,_ip,_ports):
@@ -14,8 +17,17 @@ class NetworkManager:
             errorIndication, errorStatus, errorIndex, self.varBinds = self.cmdGen.getCmd(
             cmdgen.CommunityData('public'),
             cmdgen.UdpTransportTarget((self.ip, port)),
-            cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0),
-            cmdgen.MibVariable('SNMPv2-MIB', 'sysUpTime', 0),
+            '.1.3.6.1.2.1.3.6.0',
+            '.1.3.6.1.2.1.1.1.0',
+            '.1.3.6.1.2.1.5.8.0',
+            '.1.3.6.1.2.1.2.1.0',
+            '.1.3.6.1.2.1.2.2.0',
+            '.1.3.6.1.2.1.4.1.0',
+            '.1.3.6.1.2.1.4.2.0',
+            '.1.3.6.1.2.1.3.7.0',
+            '.1.3.6.1.2.1.3.1.0',
+            '.1.3.6.1.2.1.3.1.0',
+            '.1.3.6.1.2.1.4.3.0',
             lookupNames=True, lookupValues=True)
 
             if errorIndication:
@@ -25,7 +37,7 @@ class NetworkManager:
             else:
                 self.devices.append(self.varBinds)
 
-        self.printDeviceInfo()
+        self.makeXml()
 
     def printDeviceInfo(self):
         for dev in self.devices:
@@ -40,6 +52,77 @@ class NetworkManager:
                 output.write(str(name) + '\n' + str(val) + '\n' )
             output.write('\n--------------\n')
 
-nm = NetworkManager('192.168.111.138',(5322,162,163))
+    def makeXml(self):
+        devices = []
+        xmlFile = open('device_info.xml','r+')
+        out = open('out','r+')
+        for dev in self.devices:
+            for name,val in dev:
+                out.write(str(val)+'&&')
+            out.write('\n')
+
+        out.close()
+        infile = open('out','r+')
+        devList = infile.readlines()
+
+        performanceData = []
+        for dev in devList:
+            performanceData = dev.split('&&')
+            
+            root = etree.Element("Device")
+            devId = etree.Element('Id')
+            devId.text = performanceData[0]
+            root.append(devId)
+
+            sysDescr = etree.Element('sysDescr')
+            sysDescr.text = performanceData[1]
+            root.append(sysDescr)
+
+            sysLocation = etree.Element('sysLocation')
+            sysLocation.text = performanceData[2]
+            root.append(sysLocation)
+
+            freePorts = etree.Element('freePorts')
+            freePorts.text = performanceData[3]
+            root.append(freePorts)
+
+            usedPorts = etree.Element('usedPorts')
+            usedPorts.text = performanceData[4]
+            root.append(usedPorts)
+
+            net = etree.Element('net')
+            netUp = etree.Element('netUp')
+            netUp.text = performanceData[5]
+
+            netDown = etree.Element('netDown')
+            netDown.text = performanceData[6]
+
+            net.append(netUp)
+            net.append(netDown)
+            root.append(net)
+
+            fanSpeed = etree.Element('fanSpeed')
+            fanSpeed.text = performanceData[7]
+            root.append(fanSpeed)
+            
+            voltage = etree.Element('voltage')
+            voltage.text = performanceData[8]
+            root.append(voltage)
+
+            temp = etree.Element('temp')
+            temp.text = performanceData[9]
+            root.append(temp)
+
+            bandLoad = etree.Element('bandLoad')
+            bandLoad.text = performanceData[10]
+            root.append(bandLoad)
+            
+            #print etree.tostring(root, pretty_print=True)
+            xmlFile.write(etree.tostring(root,pretty_print=True))
+
+        #print performanceData
+
+'''--------------------------debug zone-------------------------'''
+nm = NetworkManager('192.168.111.138',(5323,5324,5324))
 nm.getDeviceInfo()
-nm.printToFile()
+#nm.printToFile()
