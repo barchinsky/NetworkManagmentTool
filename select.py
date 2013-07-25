0e93d5dc9c49e55ee2ca136ab257bd7c679386e0
kliw4utsky <kliw4utsky@gmail.com>
2013-07-25 17:15:50 +0300

    select traps

---
+++ select.py
@@ -0,0 +1,19 @@
+import cx_Oracle
+
+con = cx_Oracle.connect('orcdb/passw0rd@192.168.111.138/orcl')
+cur = con.cursor()
+trap = {'lowVolatage','fanStatus Fail','temp Hight','linkDown','shutDown','coldStart','hardReset','CRC Frame','DDos detected','hard Reset','fanFail'}
+Id = raw_input("please input devices CLLI: ")
+cur.execute("select TRAP from SYSTEM.TRAP WHERE IDDEVICE=:Id",{'Id':Id})
+data = cur.fetchall()
+if data == []:
+    print 'No such device in DB'
+else:
+    print 'Errors: '
+    for row in data :
+        if row[0] in trap:
+            print row[0]
+cur.close()
+con.close()
+
+
