1487db989e6bd79722fe4610caf7dec69e77d6d3
kliw4utsky <kliw4utsky@gmail.com>
2013-07-25 17:55:21 +0300

    add select trap

--- select.py
+++ select.py
@@ -1,19 +1,23 @@
 import cx_Oracle
+from ConfigManager import ConfigManager
 
-con = cx_Oracle.connect('orcdb/passw0rd@192.168.111.138/orcl')
-cur = con.cursor()
-trap = {'lowVolatage','fanStatus Fail','temp Hight','linkDown','shutDown','coldStart','hardReset','CRC Frame','DDos detected','hard Reset','fanFail'}
-Id = raw_input("please input devices CLLI: ")
-cur.execute("select TRAP from SYSTEM.TRAP WHERE IDDEVICE=:Id",{'Id':Id})
-data = cur.fetchall()
-if data == []:
-    print 'No such device in DB'
-else:
-    print 'Errors: '
-    for row in data :
-        if row[0] in trap:
-            print row[0]
-cur.close()
-con.close()
+def Select_trap():
+    ob = ConfigManager()
+    con = cx_Oracle.connect(ob.getDBConnection())
+    cur = con.cursor()
+    trap = {'lowVolatage','fanStatus Fail','temp Hight','linkDown','shutDown','coldStart','hardReset','CRC Frame','DDos detected','hard Reset','fanFail'}
+    Id = raw_input("please input devices CLLI: ")
+    cur.execute("select TRAP from SYSTEM.TRAP WHERE IDDEVICE=:Id",{'Id':Id})
+    data = cur.fetchall()
+    if data == []:
+        print 'No such device in DB'
+    else:
+        print 'Errors: '
+        for row in data :
+            if row[0] in trap:
+                print row[0]
+    cur.close()
+    con.close()
 
+Select_trap()
 
