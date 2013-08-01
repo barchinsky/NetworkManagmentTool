4114266d5f2954c39e172f88b5967340570382ca
barchinsky <barchinsky@gmail.com>
2013-08-01 13:17:15 +0300

    Change store data order.

--- src/ServiceSimulator.py
+++ src/ServiceSimulator.py
@@ -12,8 +12,9 @@ class ServiceSimulator:
     
     def generateVoIP(self):
         voip_file = open("voip.csv","a+")
-        
         sep = ","
+
+        user_id = "user id"
         ts = str(time.time())
         delay = str(random.uniform(0.1,10)) 
         sip1 = "user_1_id" 
@@ -24,13 +25,14 @@ class ServiceSimulator:
         ts_start = ts
         ts_end = str( (float(ts_start) + random.uniform(10,500)) )
         
-        voip_file.write(ts+sep+delay+sep+sip1+sep+sip2+sep+codec+sep+echo+sep+pack_loss+sep+ts_start+sep+ts_end+'\n')
+        voip_file.write(user_id+sep+sip1+sep+sip2+sep+delay+sep+echo+sep+codec+sep+pack_loss+sep+ts+sep+ts_start+sep+ts_end+'\n')
         voip_file.close()
 
     def generateIPTV(self):
         iptv_file = open("iptv.csv","a+")
         sep = ","
  
+        user_id = "user id"
         ts = str(time.time())
         chanel = str(random.uniform(10000,20000))
         stb_id = "some_stb_id"
@@ -41,13 +43,14 @@ class ServiceSimulator:
         bandwidth = str(random.randint(0,100))
         codec = "codec_type"
 
-        iptv_file.write(ts+sep+chanel+sep+stb_id+sep+prev_chanel+sep+payment+sep+chan_spec+sep+parent+bandwidth+codec+'\n')
+        iptv_file.write(user_id+sep+chanel+sep+stb_id+sep+prev_chanel+sep+payment+sep+chan_spec+sep+parent+sep+bandwidth+sep+codec+'\n')
         iptv_file.close()
 
     def generateBroadband(self):
         bb_file = open("bb.csv",'a+')
         sep = ","
 
+        customer_info = "customer info"
         ts = str(time.time())
         delay = str(random.uniform(0,10))
         pack_loss = str(random.randint(0,150))
@@ -56,9 +59,9 @@ class ServiceSimulator:
         ip = "ip address"
         mac = "mac address"
         user_id = "user id"
-        client = "client type"
+        client_agent = "client type"
 
-        bb_file.write(ts+sep+delay+sep+pack_loss+sep+up_speed+sep+down_speed+sep+ip+sep+mac+sep+user_id+sep+client+'\n')
+        bb_file.write(user_id+sep+ip+sep+mac+sep+up_speed+sep+down_speed+sep+delay+sep+pack_loss+sep+customer_info+sep+close+sep+ts+'\n')
         bb_file.close()
 
 
