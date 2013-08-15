18d34e162d577162fdb8549a2545efeadde30a30
barchinsky <barchinsky@gmail.com>
2013-08-15 15:53:27 +0300

    Added absolute path to files(dangerously).

--- src/sqlldr.py
+++ src/sqlldr.py
@@ -1,18 +1,19 @@
+#!/usr/bin/env python
 import subprocess
 
-subprocess.call('/usr/lib/oracle/11.2/client64/bin/sqlldr orcdb/passw0rd@//192.168.111.138/orcl control=loadTV.ctl', shell=True)
-f = open('../data/iptv.csv','w')
+subprocess.call('/usr/lib/oracle/11.2/client64/bin/sqlldr orcdb/passw0rd@//192.168.111.138/orcl control=/home/max/TF/NetworkManagmentTool/src/loadTV.ctl', shell=True)
+f = open('/home/max/TF/NetworkManagmentTool/data/iptv.csv','w')
 f.write('')
 f.close()
 
-subprocess.call('/usr/lib/oracle/11.2/client64/bin/sqlldr orcdb/passw0rd@//192.168.111.138/orcl control=loadVO.ctl', shell=True)
+subprocess.call('/usr/lib/oracle/11.2/client64/bin/sqlldr orcdb/passw0rd@//192.168.111.138/orcl control=/home/max/TF/NetworkManagmentTool/src/loadVO.ctl', shell=True)
 
-f = open('../data/voip.csv','w')
+f = open('/home/max/TF/NetworkManagmentTool/data/voip.csv','w')
 f.write('')
 f.close()
 
-subprocess.call('/usr/lib/oracle/11.2/client64/bin/sqlldr orcdb/passw0rd@//192.168.111.138/orcl control=loadBB.ctl', shell=True)
+subprocess.call('/usr/lib/oracle/11.2/client64/bin/sqlldr orcdb/passw0rd@//192.168.111.138/orcl control=/home/max/TF/NetworkManagmentTool/src/loadBB.ctl', shell=True)
 
-f = open('../data/bb.csv','w')
+f = open('/home/max/TF/NetworkManagmentTool/data/bb.csv','w')
 f.write('')
 f.close()
