e816b0b28ef4746f36b5071009541f40fd01dfd1
barchinsky <barchinsky@gmail.com>
2013-08-15 15:58:26 +0300

    Added configs support. Removed while(True) cycle.

--- src/TrapGen.py
+++ src/TrapGen.py
@@ -7,6 +7,7 @@ import datetime
 import random
 import time
 import cx_Oracle
+from ConfigManager import *
 #from print_cursor import PrintCursor
 
 class TrapGen:
@@ -14,7 +15,7 @@ class TrapGen:
         self.data = ""
         self.dictionary = {}
         self.dev_cilly = self.getDeviceCLLI()
-
+        self.cm = ConfigManager()
         self.getDictionary()
 
     def send_trap(self):
@@ -24,7 +25,7 @@ class TrapGen:
 
         ntfOrg.sendNotification(
         ntforg.CommunityData('public'),
-        ntforg.UdpTransportTarget(('192.168.111.108', 5050)),
+        ntforg.UdpTransportTarget((self.cm.getTrapIp(), 5050)),
         'trap',
         ntforg.MibVariable('SNMPv2-MIB', 'sysLocation'),
         ('1.3.6.1.6.3.1.1.5.4', v2c.OctetString(self.data)))
@@ -33,14 +34,11 @@ class TrapGen:
 
     def generate(self):
         ts = time.time()
-        #st = datetime.datetime#.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-        #dev_id = ('UA5120001RT','UA5120002RT','UA5120003RT','UA5120004RT')
 
         self.data = str(self.dev_cilly[ random.randint( 0,len(self.dev_cilly)-1 ) ]) +'|'+ str(self.dictionary[random.randint(0,13)])+'|'+str(ts)
 
     def getDictionary(self):
-        with open("data/traps.txt",'r') as infile:
+        with open("/home/max/TF/NetworkManagmentTool/data/traps.txt",'r') as infile:
             for line in infile:
                 key,value = line.split(':')
                 self.dictionary[int(key)] = str(value).rstrip()
@@ -52,15 +50,11 @@ class TrapGen:
         data = []
         cur = con.cursor()
         cur.execute("select ID from SYSTEM.DEVICE")
-        #data = cur.fetchall()
         for item in cur.fetchall():
-            #print item[0]
             data.append(item[0])
         return data
 
 if __name__ == '__main__':
     obj = TrapGen()
-    while(1):
+    for i in xrange(50):
         obj.send_trap()
-        #time.sleep(random.randint(5,10))
-        #obj.getDeviceCLLI()
